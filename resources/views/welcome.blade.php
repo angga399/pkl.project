69581b266a273a4645db1241128b1f3a6b22f372
angga399 <saputra.angga.qi@gmail.com>
2024-10-15 17:08:47 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+@vite('resources/css/app.css')
 <nav class="bg-gray-800"  x-data="{isOpen: false}"> 
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 items-center justify-between">
