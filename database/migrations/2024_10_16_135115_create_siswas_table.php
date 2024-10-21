9144cf57e09709eb08fed9cee5b751281de6814c
angga399 <saputra.angga.qi@gmail.com>
2024-10-21 11:11:14 +0700

    llj
    holh

---
+++ database/migrations/2024_10_16_135115_create_siswas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('hari_tanggal');
+            $table->string('jam_datang');
+            $table->string('jam_pulang');
+            $table->string('paraf_pembimbing');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
