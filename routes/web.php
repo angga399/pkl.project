8a7e34bfe196fafd44e23e4b5aa628acfb89a81b
angga399 <saputra.angga.qi@gmail.com>
2024-11-01 23:04:02 +0700

    ,m
    ,lkm

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,9 @@ use App\Http\Controllers\JournalController;
 use App\Http\Controllers\DaftarhdrController;
 use App\Http\Controllers\AbsenController;
 
-
-
-Route::get('/absen', [AbsenController::class, 'index'])->name('absen.index');
-Route::post('/absen', [AbsenController::class, 'store'])->name('absen.store');
-
-
 Route::get('/welcome', function () {
     return view('welcome'); // pastikan ada file 'welcome.blade.php' di resources/views
 })->name('welcome');
-
-Route::resource('daftarhdr', DaftarhdrController::class);
 
 Route::post('/create', [DaftarhdrController::class, 'store'])->name('create.store');
 Route::resource('journals', JournalController::class);
