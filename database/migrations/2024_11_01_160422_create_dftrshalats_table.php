30e966892e1b409ef417eedf389e17a5a75e26f1
angga399 <saputra.angga.qi@gmail.com>
2024-11-02 19:05:11 +0700

    Update 2024_11_01_160422_create_dftrshalats_table.php

--- database/migrations/2024_11_01_160422_create_dftrshalats_table.php
+++ database/migrations/2024_11_01_160422_create_dftrshalats_table.php
@@ -1,11 +1,10 @@
-<?php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateDftrshalatsTable extends Migration
 {
-    public function up(): void
+    public function up()
     {
         Schema::create('dftrshalats', function (Blueprint $table) {
             $table->id();
@@ -18,8 +17,8 @@ return new class extends Migration
         });
     }
 
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('dftrshalats');
     }
-};
+}
