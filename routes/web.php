4634ae76708bbb03d943b97cd0b7b3d0e40e5730
angga399 <saputra.angga.qi@gmail.com>
2025-01-08 08:14:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,27 +7,15 @@ use App\Http\Controllers\DftrshalatController;
 use App\Http\Controllers\PembimbingController;
 use App\Http\Controllers\PembimbingdController;
 use App\Http\Controllers\ShalatController;
-// Rute utama untuk daftarhdr
-Route::resource('daftarhdr', DaftarhdrController::class); // Ini sudah mencakup semua rute untuk daftarhdr
 
-// Rute lainnya
+// Rute utama untuk daftarhdr
+Route::resource('daftarhdr', DaftarhdrController::class);
+
+// Rute untuk dftrshalats
 Route::resource('dftrshalats', DftrshalatController::class);
 
-
-
-
-
-Route::get('/pembimbingpkl', function () {
-    return view('pembimbingpkl'); // pastikan ada file 'pembimbingpkl.blade.php' di resources/views
-})->name('pembimbingpkl');
-
-
-Route::post('/create', [DaftarhdrController::class, 'store'])->name('create.store');
-
-Route::resource('journals', JournalController::class);
-
 // Halaman utama
-Route::get('/welcome', function () {
+Route::get('/', function () {
     return view('welcome', ['title' => 'home page']);
 })->name('welcome');
 
@@ -37,14 +25,17 @@ Route::post('/journals/{id}/setuju', [PembimbingController::class, 'setuju'])->n
 Route::post('/journals/{id}/tolak', [PembimbingController::class, 'tolak'])->name('pembimbing.tolak');
 
 // Rute untuk pembimbingd (halaman persetujuan)
-
 Route::get('/pembimbingd/index', [PembimbingdController::class, 'index'])->name('pembimbingd.index');
 Route::post('/pembimbingd/{id}/setuju', [PembimbingdController::class, 'approve'])->name('pembimbingd.approve');
 Route::post('/pembimbingd/{id}/tolak', [PembimbingdController::class, 'not approve'])->name('pembimbingd.not approve');
 
+// Rute untuk halaman pembimbingpkl
+Route::get('/pembimbingpkl', function () {
+    return view('pembimbingpkl'); // pastikan ada file 'pembimbingpkl.blade.php' di resources/views
+})->name('pembimbingpkl');
 
+// Rute untuk journals
+Route::resource('journals', JournalController::class);
 
-Route::get('/', [DftrshalatController::class, 'index'])->name('dftrshalats.index');
-Route::get('/create', [DftrshalatController::class, 'create'])->name('dftrshalats.create');
-Route::post('/store', [DftrshalatController::class, 'store'])->name('dftrshalats.store');
-
+// Rute default untuk aplikasi
+Route::get('/dftrshalats', [DftrshalatController::class, 'index'])->name('dftrshalats.index');
